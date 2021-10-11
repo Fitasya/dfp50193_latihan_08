a330d7a3dc54c6d9d4eb2044bce0c78762bb0cec
As Syafaa <purpleeisya@gmail.com>
2021-10-11 12:45:19 +0800

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array</title>
+    <style>
+        * {
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            font-size: 30px;
+        }
+        </style>
+</head>
+<body>
+    <h1>Array</h1>
+    [
+        <a href="index.php">Numeric Array</a>
+        <a href="assoc.php">Associative Array</a>
+        <a href="multi.php">Multidimensional Array</a>
+        <a href="produk.php">Produk</a>
+        ]
+        <hr>
+</body>
+</html>
